00ea5ea8ad98430ab3124d9d2f87a1ea5c9e45a4
Daniel Barreto <danielfsbarreto@gmail.com>
2026-03-26 16:05:39 -0300

    Move back to old approach

--- src/flight_concierge/main.py
+++ src/flight_concierge/main.py
@@ -18,9 +18,7 @@ tracer_provider = register(
     api_key=os.getenv("ARIZE_API_KEY"),
     project_name=os.getenv("ARIZE_PROJECT_NAME"),
 )
-CrewAIInstrumentor().instrument(
-    tracer_provider=tracer_provider, use_event_listener=True, create_llm_spans=False
-)
+CrewAIInstrumentor().instrument(tracer_provider=tracer_provider)
 OpenAIInstrumentor().instrument(tracer_provider=tracer_provider)
 
 
